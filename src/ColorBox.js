aae80681fe7a84d38b6723ebd9b0bed00fc8baf6
Saint-Jean Germila <germila@noukod.com>
2019-04-22 19:21:18 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,8 +4,15 @@ export default class ColorBox extends Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <div className="color-box" style={{opacity:this.props.opacity}}>
         {this.props.opacity > 0.2 ? <ColorBox opacity={this.props.opacity - 0.1}/>:""}
+=======
+      <div className="color-box" style="opacity: 1;">
+      <div className="color-box" style="opacity: 0.9;">
+      <div className="color-box" style="opacity: 0.8;">
+        {/* your conditional code here! */}
+>>>>>>> e4a9c567efbe481505fceb6c3835153e99bc31a0
       </div>
     )
   }
--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React,{ Component } from 'react';
 
 class Comment extends  Component{
@@ -13,3 +14,14 @@ class Comment extends  Component{
 }
 
 export default Comment;
+=======
+import React, { Component } from 'react'
+import BlogPost from './BlogPost.js'
+
+class X extends Component{
+  render() {
+    <BlogPost/>
+  }
+}
+// add Comment component here
+>>>>>>> e4a9c567efbe481505fceb6c3835153e99bc31a0
